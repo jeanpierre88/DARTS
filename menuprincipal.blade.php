bf1dc6db0a90d73d8fde0760e45d2d430dc5dc40
Ryussei <38542447+Ryussei@users.noreply.github.com>
2018-05-10 15:05:28 -0500

    Add files via upload

---
+++ menuprincipal.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/menup.css') }}" />
+        <title>MENU</title>
+    </head>
+    <body>
+
+        <header id = "cabecera">
+            <h1 id="titulo">MENU PRINCIPAL</h1>
+        </header>
+
+        <section id = "menu">
+          <ul>
+            <li><a>SELECCION DE TORNEO</a></li>
+            <li><a>SELECCION DE NIVEL</a></li>
+            <li><a>INGRESAR JUGADOR</a></li>
+            <li><a>SALIR</a></li>
+          </ul>
+        </section>
+
+      {{-- <footer id = "pie">
+        © Derechos Reservados<br>Ingeniería de Sistemas
+      </footer> --}}
+
+    </body>
+</html>
